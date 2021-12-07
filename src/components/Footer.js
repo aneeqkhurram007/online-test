214436ae48ec706e3e0065a9dd5a57a3615b9536
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-12-07 23:10:06 +0500

    👌 IMPROVE: Time added in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
 
 const Footer = () => {
-  const [time, settime] = useState(Date.now());
+  const [time, settime] = useState(new Date().toLocaleTimeString());
   useEffect(() => {
-    settime(Date.now());
-  });
+    setTimeout(() => {
+      settime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, [time]);
   return (
-    <div className="flex justify-between w-full rounded-t-lg p-4 h-28 bg-red-500">
+    <div className="flex justify-between w-full rounded-t-lg p-4 h-28 bg-yellow-500">
       <h1 className="font-bold text-lg">
         Date: {new Date().toLocaleDateString()}
       </h1>
-      <h1>Time: {time}</h1>
+      <h1 className="font-bold text-lg">Time: {time}</h1>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import NavButton from "./NavButton";
 
 const Header = () => {
   return (
-    <div className="w-full flex justify-between p-4 text-2xl font-bold text-center">
+    <div className="w-full flex justify-between p-4 bg-yellow-400 text-2xl font-bold text-center">
       <h1>Test Online</h1>
       <div className="space-x-2">
         {Array(3)
