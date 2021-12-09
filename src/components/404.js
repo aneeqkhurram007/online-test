2a0f6a13234bec6ad5f6ae3850cb6bd67ba17548
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-12-09 07:44:26 +0500

    👌 IMPROVE: Time-section

--- src/components/404.js
+++ src/components/404.js
@@ -1,5 +1,14 @@
 const Error = () => {
-  return <div>404Error</div>;
+  return (
+    <div>
+      <h1 className="text-center text-yellow-500 text-4xl font-semibold">
+        <span className="text-red-600 text-6xl">404</span> | Error
+      </h1>
+      <h2 className="text-yellow-500 font-semibold mt-2 text-lg">
+        Page not found
+      </h2>
+    </div>
+  );
 };
 
 export default Error;
--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -12,7 +12,7 @@ const TestPage = () => {
     }, 1000);
   }, [timer, dispatch]);
   return (
-    <div className="flex w-11/12 border border-black shadow-lg h-4/6 rounded-lg">
+    <div className="flex w-11/12 shadow-lg h-4/6 rounded-lg">
       <section className="w-1/6 border bg-yellow-200 border-black h-full rounded-l-lg ">
         <div
           className="flex flex-col w-full
@@ -22,14 +22,28 @@ const TestPage = () => {
           <h2 className="font-semibold">Remaining Time</h2>
           <h3>00:00:{timer}</h3>
         </div>
-        <div className="grid grid-cols-3 p-2 border border-black">
+        <div className="grid grid-cols-3 p-2">
           {Array(30)
             .fill(0)
             .map((i, index) => (
-              <div className="space-x-1 border border-black bg-white text-center">
+              <div className=" rounded-lg border border-black bg-white text-center">
                 {index}
               </div>
             ))}
+        </div>
+        <div className="w-full flex flex-row justify-between text-xs">
+          <div className="flex flex-col">
+            <p>Current</p>
+            <div className="w-4 h-4 rounded-lg ml-4 bg-yellow-600"></div>
+          </div>
+          <div className="flex flex-col">
+            <p>Answered</p>
+            <div className="w-4 h-4 rounded-lg ml-4 bg-green-600"></div>
+          </div>
+          <div className="flex flex-col">
+            <p>Unanswered</p>
+            <div className="w-4 h-4 rounded-lg ml-4 bg-red-600"></div>
+          </div>
         </div>
       </section>
       <section className="flex-1 border border-black h-full rounded-r-lg "></section>
